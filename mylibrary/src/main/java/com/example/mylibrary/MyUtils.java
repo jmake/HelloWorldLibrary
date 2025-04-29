867dff57d57a33c06ec097a1621aaea0a65b6ed9
jmake <zvl_2006@hotmail.com>
2025-04-29 20:21:12 +0200

    Update and rename src/main/java/com/hello/world/SpicyTech.java to mylibrary/src/main/java/com/example/mylibrary/MyUtils.java

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyUtils.java
@@ -0,0 +1,7 @@
+package com.example.mylibrary;
+
+public class MyUtils {
+    public static String sayHello() {
+        return "Hello from MyLibrary!";
+    }
+}
--- src/main/java/com/hello/world/SpicyTech.java
+++
@@ -1,9 +0,0 @@
-package com.hello.world;
-
-public class SpicyTech 
-{
-    public static String greet() 
-    {
-        return "[SpicyTech] Hello!!";
-    }
-}
